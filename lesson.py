45dc4801aa1f7e43d7362fbbb1f4a460f1ae2504
soo <sujinsnla@gmail.com>
2022-02-25 23:16:05 +0900

    .

--- lesson.py
+++ lesson.py
@@ -1,3 +1,6 @@
+from xmlrpc.client import boolean
+
+
 print(5)
 print(-10)
 print(3.14)
@@ -5,4 +8,47 @@ print(1000)
 print(5+3)
 print(2*8)
 print(3*(3+1))
+print('풍선')
+print("나비")
+
+#boolean 참/거짓
+
+print(5>10)
+print(5<10)
+print(True)
+print(False)
+print(not True)
+print(not False)
+print(not(5>10))
+
+#변수/ 애완동물을 소개해주세요~
+animal = "강아지"
+name = "연탄이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+#위의 변수를 바꿔가며 아래 코드를 사용 할 수 있다
+
+
+print("우리집에서 "+ animal +"의 이름은 "+name+"에요")
+print(name+"는 "  + str(age) +"살이며," +hobby+"을 아주 좋아해요")
+
+print(name+"는" ,age,"살이며,",hobby,"을 아주 좋아해요")
+
+
+print(name+"는 어른일까요?" + str(is_adult))
+
+#quiz) 변수를 이용하여 다음 문자을 출력하시오
+
+# 변수명 :station
+ 
+station = "사당"
+# ,"신도림","인천공항"
+
+#출력문장 : xx행 열차가 들어오고 있습니다.
+print(station+"행 열차가 들어오고 있습니다.")
+
+
+
+
 
