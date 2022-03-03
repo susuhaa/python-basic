33aa1c5ad0c7be0ae10a0dfde6fde739bdda8e98
soo <sujinsnla@gmail.com>
2022-03-04 00:41:26 +0900

    함수

---
+++ function/human profile.py
@@ -0,0 +1,18 @@
+def profile(name, age, main_lang):
+    print("이름 : {0}\t나이 : {1}\t 주 사용언어: {2}"\
+        .format(name,age, main_lang))
+
+profile("유재석", 20, "파이썬")
+profile("김태호",25,"자바")
+
+
+# 같은 학교 같은 학년 같은반 같은 수업...
+
+
+def profile(name, age=17, main_lang ="파이썬"):
+    print("이름 : {0}\t나이 : {1}\t 주 사용언어: {2}"\
+        .format(name,age, main_lang))
+
+
+profile("유재석", 20, "파이썬")
+profile("김태호")
