0164c1da71c23d5912d2f797832748ff894399d6
soo <sujinsnla@gmail.com>
2022-03-06 15:19:31 +0900

    파일입출력

---
+++ 파일 입출력/파일 입출력2.py
@@ -0,0 +1,16 @@
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+
+score_file = open("score.txt", "r", encoding="utf8")
+lines = score_file.readlines() # list 형태로 저장
+for line in lines:
+    print(line, end="")
+
+score_file.close()
+
